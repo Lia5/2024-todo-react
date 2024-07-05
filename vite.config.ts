08d360ea6170e30178e40506450f75d93391c6f1
lia5 <liaforweb@gmail.com>
2024-07-05 06:33:10 +0300

    change: lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// @ts-ignore
-import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    eslintPlugin()],
+  plugins: [react()],
 })
